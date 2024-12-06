0bb0c5750dbb3def4abf64b2121e5f8e1d2cd3f3
STEVEN A BOWMAN <stevenabowman@STEVENs-MacBook-Pro.local>
2024-12-06 15:14:44 -0500

    Server No Secret Key No Secret
     Changes to be committed:
    	deleted:    server/.env

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -7,7 +7,7 @@ dotenv.config();
 const router = express.Router();
 
 const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.,
 });
 
 const openai = new OpenAIApi(config);
