76bd92cfa4bb7764787887c13ba31fda446101a2
STEVEN A BOWMAN <stevenabowman@STEVENs-MacBook-Pro.local>
2024-12-06 10:34:32 -0500

    ColorPicker

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,10 +1,9 @@
 import React, {useState, useEffect } from 'react';
 import {AnimatePresence, motion } from 'framer-motion';
 import { useSnapshot } from 'valtio';
-
 import config from '../config/config';
 import state from '../store';
-import { download } from '../assets';
+import { download, stylishShirt } from '../assets';
 import { downloadCanvasToImage, reader } from '../config/helpers';
 import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
 import { fadeAnimation, slideAnimation } from '../config/motion';
@@ -13,6 +12,32 @@ import { AIPicker, ColorPicker, CustomButton,
 
 const Customizer = () => {
   const snap = useSnapshot(state);
+
+  const [file, setFile] = useState('');
+
+  const [prompt, setPrompt] = useState('');
+  const [generatingImg, setGeneratingImg] = useState(false);
+  
+  const [activeEditorTab, setActiveEditorTab] = useState('');
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true, 
+    stylishShirt: false, 
+  })
+
+  // show tab content depending on the active tab
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case "colorpicker":
+        return <ColorPicker />
+      case "filepicker":
+        return <FilePicker />
+      case "aipicker":
+        return <AIPicker />
+      default:
+        return null;
+    }
+
+  }
 
   return (
     <AnimatePresence>
@@ -29,9 +54,11 @@ const Customizer = () => {
                 <Tab
                   key={tab.name}
                   tab={tab}
-                  handleClick={() => {}}
+                  handleClick={() => setActiveEditorTab(tab.name)}
                 />
               ))}
+              
+              {generateTabContent()}
             </div>
           </div>
         </motion.div>
